bdfbb84c1ac749cf84922b7f801f571c410f913b
Samuel Popoola <samuelpopoola323@gmail.com>
2025-08-28 09:21:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
     origin: [
       "http://localhost:5500",            // local static hosting
       "http://127.0.0.1:5500",
-      "https://harvesters-hub.vercel.app/", // e.g. Vercel domain
+      "https://harvesters-hub.vercel.app", // e.g. Vercel domain
     ],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
